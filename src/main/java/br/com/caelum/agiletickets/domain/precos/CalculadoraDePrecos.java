6b578cb18daae29fb76c9fdbb12a5579a593564c
Paulo <paulo.kun@gmail.com>
2017-08-29 12:26:56 -0300

    uso do multiplicador

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -9,28 +9,30 @@ public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco = sessao.getPreco(); 
+		BigDecimal multiplicador = new BigDecimal(0);
 		
 		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
 			//quando estiver acabando os ingressos... 
 			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
+				multiplicador = BigDecimal.valueOf(0.10);
 			}
 		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
 			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+				multiplicador = BigDecimal.valueOf(0.20);
 			}
 			
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			}
 		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
 			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
+				multiplicador = BigDecimal.valueOf(0.20);
 			}
-
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			}
+		}
+		
+		preco = sessao.getPreco().add(sessao.getPreco().multiply(multiplicador));
+		
+		if(sessao.getDuracaoEmMinutos() != null && sessao.getDuracaoEmMinutos() > 60
+				&& (sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)
+				|| sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) ){
+			preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 		}
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
